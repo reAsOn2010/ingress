eda067a410dd61234e1499e8a088b9c2c4d6c1c1
reAsOn2010 <the.reason.sake@gmail.com>
2016-08-20 11:52:32 +0800

    add deploy example and make more configurable

--- controllers/haproxy/haproxy/config/config.go
+++ controllers/haproxy/haproxy/config/config.go
@@ -6,14 +6,28 @@ var ()
 
 // Configuration represents the content of haproxy.cfg file
 type Configuration struct {
-	EnableSyslog bool `structs:"enableSyslog,omitempty"`
+	EnableSyslog         bool `structs:"enableSyslog,omitempty"`
+	HttpRequestTimeout   int  `structs:"httpRequestTimeout"`
+	ConnectTimeout       int  `structs:"connectTimeout"`
+	ClientTimeout        int  `structs:"clientTimeout"`
+	ClientFinTimeout     int  `structs:"clientFinTimeout"`
+	ServerTimeout        int  `structs:"serverTimeout"`
+	TunnelTimeout        int  `structs:"tunnelTimeout"`
+	HttpKeepAliveTimeout int  `structs:"httpKeepAliveTimeout"`
 }
 
 // NewDefault returns the default configuration contained
 // in the file default-conf.json
 func NewDefault() Configuration {
 	cfg := Configuration{
-		EnableSyslog: false,
+		EnableSyslog:         false,
+		HttpRequestTimeout:   5,
+		ConnectTimeout:       5,
+		ClientTimeout:        50,
+		ClientFinTimeout:     50,
+		ServerTimeout:        50,
+		TunnelTimeout:        3600,
+		HttpKeepAliveTimeout: 60,
 	}
 
 	return cfg
